dbccc8e312e136afb2d4f2b69a135a48562771d9
John Faber <john.florez25@gmail.com>
2020-07-11 18:27:44 -0500

    Added selectors, filter by id and publisher heroes

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { HeroList } from '../heroes/HeroList'
 
 export const DcScreen = () => {
     return (
         <div>
             <h1>DCScreen</h1>
+            <hr />
+
+            <HeroList publisher="DC Comics" />
         </div>
     )
 }
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from '../components/ui/Navbar';
 import { LoginScreen } from '../components/login/LoginScreen';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { DashboardRoutes } from './DashboardRoutes';
+
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Navbar />
         <Switch>
           <Route exact path='/login' component={LoginScreen} />
-          <Route exact path='/' component={MarvelScreen} />
+          <Route path='/' component={DashboardRoutes} />
         </Switch>
       </div>
     </Router>
