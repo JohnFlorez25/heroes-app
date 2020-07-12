67d7db63e9c905d71746e3522a85412b162c89d0
John Faber <john.florez25@gmail.com>
2020-07-11 23:04:36 -0500

    updated assets routes

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -32,7 +32,7 @@ export const HeroScreen = ({ history }) => {
     <div className='row mt-5'>
       <div className='col-4'>
         <img
-          src={`../assets/heroes/${heroId}.jpg`}
+          src={`./assets/heroes/${heroId}.jpg`}
           className='img-thumbnail animate__animated animate__fadeInLeft'
           alt={superhero}
         />
