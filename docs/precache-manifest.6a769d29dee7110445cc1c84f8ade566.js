cbce909833e2f75e86b525e769bafc843f4af755
John Faber <john.florez25@gmail.com>
2020-07-11 23:30:22 -0500

    docs

---
+++ docs/precache-manifest.6a769d29dee7110445cc1c84f8ade566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f9cfd2bb139520c5d8dad25583b4f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc39c7d1f7c6c58c56d7",
+    "url": "/static/js/2.c8b3cc8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8b3cc8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6289ed0f226e1fc063d6",
+    "url": "/static/js/main.cad23b12.chunk.js"
+  },
+  {
+    "revision": "7812fe34c6666eb2c1e5",
+    "url": "/static/js/runtime-main.59f6df52.js"
+  }
+]);
